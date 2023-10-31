4d706c2aced7e4f9851f1bdaf038c50b4aa3df0a
TheValll <81711481+TheValll@users.noreply.github.com>
2023-10-31 14:56:56 +0100

    fetch fix
    fetch fix

--- script.js
+++ script.js
@@ -40,8 +40,6 @@ const countryInfo = async () => {
   const region = document.getElementById("region");
   const capital = document.getElementById("capital");
   const population = document.getElementById("population");
-  // const language = document.getElementById("language");
-  // const currencies = document.getElementById("currencies");
 
   let country = inputValue.value;
 
@@ -84,9 +82,7 @@ const countryInfo = async () => {
 
 const pinFunct = async () => {
   activeMap = 1;
-  await fetch(
-    `https://m218.syncusercontent.com/mfs-60:654c50e09404d9c7e7e8f52d5e728ae3=============================/p/aeroports.json?allowdd=0&datakey=FSaUOVbsGH2mFtgIkaTuQPlj3ueNXJuXh+JmycOHa85o4qnyYVfIWF8Npmj/duNm9hhg8fX5wZKPATUPR5bkGugDNqfkSu0RV9R2EnbG/gw5ttxX17b4uDnGKG0umWzH56jIuQqTalxHBIFa5lP0qwM3DuTL3u5UMxkb8ReFKvfEpymJquvygQJTzupt2ytsnPLWLLftQcjKuepZXVztmmq/CB1hJvHKJ+fr3JwbofF97UTbYJj77vLcqf4Z5VgjIXKLXGdm6ZrA/zO1Iek0d4sH74QBqTCULW7V5topDeridvyqaGFPFcMNT64ZYmTFuxNuN30ea9o/3zGstuATlA&engine=ln-3.1.38&errurl=TUQIfBXpNqAHVXKXDJY9iNJw8GckXg3XIiBci+6Q3bGtjp1vXkzn49FakwukCRqbjOQZ84ovj6EMLbB1L7/Ijk2K5eQqKLm2rMjBcHcrSApCfdwKaFVaaD0r6ZqsoQ4cyy9pJ4O8+FduFpX3jsoqtH0JDc5GNzH8C1OMmg6pCs/voOLnxqdVnv/2ZSNYC+/+dwd5bixlPW4MTgaqlfN27UYDMXdLu1fp5HS38EGH7aVMh8m35G2qPrRG47IXMk9upluJA3qMcnpDIE0jRNWIqFauF/jKYW/oNsOBlhqNW/6sG5x2F75eCmdYcmfKoi/F1BljyfZGlECTuwjZCLLUSg==&header1=Q29udGVudC1UeXBlOiB0ZXh0L3BsYWlu&header2=Q29udGVudC1EaXNwb3NpdGlvbjogaW5saW5lOyBmaWxlbmFtZT0iYWVyb3BvcnRzLmpzb24iO2ZpbGVuYW1lKj1VVEYtOCcnYWVyb3BvcnRzLmpzb247&ipaddress=3277191185&linkcachekey=c35eaa330&linkoid=2168150011&mode=101&sharelink_id=14722538380011&timestamp=1698306026964&uagent=80b12c334bbd7b97cf291509cc39c7f38f38fea6&signature=c8e6abcbc07ab2f37ac89048c69f50716ab29cdb&cachekey=60:654c50e09404d9c7e7e8f52d5e728ae3=============================`
-  )
+  await fetch(`https://thevalll.github.io/airport-data-base/aeroports.json`)
     .then((res) => {
       if (!res.ok) {
         return;
